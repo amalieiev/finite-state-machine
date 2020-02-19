cf67cd9eb9f37f9697ca2434fcc46f85afa1803b
artem.malieiev <artem.malieiev@gmail.com>
2020-02-19 11:42:30 +0200

    Added 'matches' API method. Added tests. Formatted core file.

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 function Machine(schema, config) {
-    let current = schema.initial
-    let callbacks = []
+  let current = schema.initial;
+  let callbacks = [];
 
-    return {
-        send: function(event, payload) {
-            const next = schema.states[current][event]
+  return {
+    send: function(event, payload) {
+      const next = schema.states[current][event];
 
-            if (next) {
-                if (next.target !== current) {
-                    current = next.target
-                    callbacks.forEach(callback => callback(current))
-                }
-
-                next.actions && next.actions.forEach(action => config.actions[action](event, payload))
-            }
-
-            return this
-        },
-
-        current: function() {
-            return current
-        },
-
-        subscribe: function (callback) {
-            callbacks.push(callback)
-        },
-
-        unsubscribe: function (callback) {
-            callbacks = callbacks.filter(fn => fn !== callback)
+      if (next) {
+        if (next.target !== current) {
+          current = next.target;
+          callbacks.forEach(callback => callback(current));
         }
+
+        next.actions &&
+          next.actions.forEach(action =>
+            config.actions[action](event, payload)
+          );
+      }
+
+      return this;
+    },
+
+    current: function() {
+      return current;
+    },
+
+    matches: function(...args) {
+      return args.includes(current);
+    },
+
+    subscribe: function(callback) {
+      callbacks.push(callback);
+    },
+
+    unsubscribe: function(callback) {
+      callbacks = callbacks.filter(fn => fn !== callback);
     }
+  };
 }
 
 module.exports = {
-    Machine
-}
+  Machine
+};
--- index.test.js
+++ index.test.js
@@ -35,67 +35,87 @@ describe('Machine', () => {
     });
   });
 
+  describe('send', () => {
+    test('should call transition actions with correct arguments', () => {
+      m1.send('SEND', { foo: 'bar' });
+      expect(sendRequest.mock.calls.length).toBe(1);
+      expect(sendRequest.mock.calls[0][0]).toBe('SEND');
+      expect(sendRequest.mock.calls[0][1]).toEqual({ foo: 'bar' });
+    });
+
+    test('should change state by receiving events', () => {
+      m1.send('SEND');
+      m1.send('SUCCESS');
+      expect(m1.current()).toBe('success');
+    });
+
+    test('should not change state if action is not presend in it', () => {
+      m1.send('SUCCESS');
+      expect(m1.current()).toBe(schema.initial);
+    });
+  });
+
+  describe('subscribe', () => {
+    test('should call provided callbacks', () => {
+      const c1 = jest.fn();
+      m1.subscribe(c1);
+      m1.send('SEND');
+      expect(c1.mock.calls.length).toBe(1);
+      expect(c1.mock.calls[0][0]).toBe('pending');
+    });
+
+    test('should not call subscribers if state is not changed', () => {
+      const callback_1 = jest.fn();
+      m1.subscribe(callback_1);
+      expect(m1.current()).toBe('idle');
+      m1.send('INIT');
+      expect(m1.current()).toBe('idle');
+      expect(callback_1.mock.calls.length).toBe(0);
+    });
+  });
+
+  describe('matches', () => {
+    test('should be a function', () => {
+      expect(m1.matches).toBeFunction();
+    });
+
+    test('should work with one value', () => {
+      expect(m1.matches('idle')).toBeTruthy();
+    });
+
+    test('should work with multiple values', () => {
+      expect(m1.matches('idle', 'success')).toBeTruthy();
+    });
+  });
+
+  describe('unsubscribe', () => {
+    test('should subscibe and unsubscribe to state changes', () => {
+      const callback_1 = jest.fn();
+      const callback_2 = jest.fn();
+      m1.subscribe(callback_1);
+      m1.subscribe(callback_2);
+      m1.send('SEND');
+      expect(callback_1.mock.calls[0][0]).toBe('pending');
+      expect(callback_2.mock.calls[0][0]).toBe('pending');
+      m1.unsubscribe(callback_1);
+      m1.send('SUCCESS');
+      expect(callback_1.mock.calls.length).toBe(1);
+      expect(callback_2.mock.calls.length).toBe(2);
+    });
+  });
+
+  test('should change state before calling actions', () => {
+    const c1 = jest.fn();
+    m1.subscribe(c1);
+    m1.send('SEND');
+    expect(c1).toHaveBeenCalledBefore(sendRequest);
+  });
+
   test('should be a function', () => {
     expect(Machine).toBeDefined();
   });
 
   test('should have correct initial state', () => {
     expect(m1.current()).toBe('idle');
-  });
-
-  test('should call transition actions with correct arguments', () => {
-    m1.send('SEND', {foo: 'bar'});
-    expect(sendRequest.mock.calls.length).toBe(1);
-    expect(sendRequest.mock.calls[0][0]).toBe('SEND');
-    expect(sendRequest.mock.calls[0][1]).toEqual({foo: 'bar'});
-  });
-
-  test('should change state by receiving events', () => {
-    m1.send('SEND')
-    m1.send('SUCCESS')
-    expect(m1.current()).toBe('success')
-  })
-
-  test('should call provided callbacks', () => {
-    const c1 = jest.fn();
-    m1.subscribe(c1);
-    m1.send('SEND');
-    expect(c1.mock.calls.length).toBe(1);
-    expect(c1.mock.calls[0][0]).toBe('pending');
-  });
-
-  test('should not change state if action is not presend in it', () => {
-    m1.send('SUCCESS');
-    expect(m1.current()).toBe(schema.initial);
-  });
-
-  test('should not call subscribers if state is not changed', () => {
-    const callback_1 = jest.fn();
-    m1.subscribe(callback_1);
-    expect(m1.current()).toBe('idle');
-    m1.send('INIT');
-    expect(m1.current()).toBe('idle');
-    expect(callback_1.mock.calls.length).toBe(0);
-  });
-
-  test('should subscibe and unsubscribe to state changes', () => {
-    const callback_1 = jest.fn();
-    const callback_2 = jest.fn();
-    m1.subscribe(callback_1);
-    m1.subscribe(callback_2);
-    m1.send('SEND');
-    expect(callback_1.mock.calls[0][0]).toBe('pending');
-    expect(callback_2.mock.calls[0][0]).toBe('pending');
-    m1.unsubscribe(callback_1);
-    m1.send('SUCCESS');
-    expect(callback_1.mock.calls.length).toBe(1);
-    expect(callback_2.mock.calls.length).toBe(2);
-  });
-
-  test('should change state before calling actions', () => {
-    const c1 = jest.fn()
-    m1.subscribe(c1)
-    m1.send('SEND')
-    expect(c1).toHaveBeenCalledBefore(sendRequest)
   });
 });
